d412850147c83f1d435d0c7dc8e4b4df619d6437
User <otarbakh@gmail.com>
2023-01-13 15:09:42 +0400

    working on viemodel 1.1

--- app/src/main/java/com/example/newsapp_v1/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/example/newsapp_v1/data/repository/NewsRepositoryImpl.kt
@@ -1,30 +1,43 @@
 package com.example.newsapp_v1.data.repository
 
 
+import com.example.newsapp_v1.common.util.Resource
 import com.example.newsapp_v1.data.NewsApi
 import com.example.newsapp_v1.data.model.Article
-import com.example.newsapp_v1.data.model.NewsResponse
 import com.example.newsapp_v1.domain.NewsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class NewsRepositoryImpl @Inject constructor(
     private val api: NewsApi
 ) : NewsRepository {
-    override suspend fun getBreakingNews(): List<Article> {
-        val response = api.getBreakingNews(query = "autosport")
-        return if (response.isSuccessful) {
-            response.body()?.articles!!
-        } else {
-            emptyList()
+
+    override suspend fun getBreakingNews(): Flow<List<Article>> = flow  {
+
+        try {
+            emit(Resource.Loading(true))
+            val response = api.getBreakingNews(query = "autosport")
+            if (response.isSuccessful) {
+                emit(Resource.Success(response.body()!!))
+            }
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "unexpected"))
         }
     }
 
-    override suspend fun getSearchNews(searchQuery:String): List<Article> {
-        val response = api.getSearchedNews(query = searchQuery)
-        return if (response.isSuccessful) {
-            response.body()?.articles!!
-        } else {
-            emptyList()
+
+    override suspend fun getSearchNews(searchQuery:String): Flow<List<Article>> = flow {
+
+        try {
+            emit(Resource.Loading(true))
+            val response = api.getSearchedNews(query = searchQuery)
+            if (response.isSuccessful) {
+                emit(Resource.Success(response.body()!!.articles))
+            }
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "unexpected"))
         }
 
     }
--- app/src/main/java/com/example/newsapp_v1/domain/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp_v1/domain/NewsRepository.kt
@@ -2,10 +2,12 @@ package com.example.newsapp_v1.domain
 
 import com.example.newsapp_v1.data.model.Article
 import com.example.newsapp_v1.data.model.NewsResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 interface NewsRepository {
 
-    suspend fun getBreakingNews(): List<Article>
+    suspend fun getBreakingNews(): Flow<List<Article>>
 
-    suspend fun getSearchNews(searchQuery:String): List<Article>
+    suspend fun getSearchNews(searchQuery: String): Flow<List<Article>>
 }
